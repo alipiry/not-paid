ad440120b729618c42eaec5d5d558df08eecf082
Ali Piry <alipiry@disroot.org>
2019-02-06 21:56:44 +0330

    Update not-paid.js
    Clean up

--- not-paid.js
+++ not-paid.js
@@ -1,17 +1,17 @@
 (function(){
 	/* change these variables as you wish */
-	const due_date = new Date('2017-02-27');
-	const days_deadline = 60;
+	const dueDate = new Date('2017-02-27');
+	const daysDeadline = 60;
 	/* stop changing here */
 	
-	const current_date = new Date();
-	const utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
-	const utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
+	const currentDate = new Date();
+	const utc1 = Date.UTC(dueDate.getFullYear(), dueDate.getMonth(), dueDate.getDate());
+	const utc2 = Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
 	const days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
 	
 	if (days > 0) {
-		const days_late = days_deadline-days;
-		const opacity = (days_late*100/days_deadline)/100;
+		const daysLate = daysDeadline - days;
+		const opacity = (daysLate * 100 / daysDeadline) / 100;
 			opacity = (opacity < 0) ? 0 : opacity;
 			opacity = (opacity > 1) ? 1 : opacity;
 		if (opacity >= 0 && opacity <= 1) {
